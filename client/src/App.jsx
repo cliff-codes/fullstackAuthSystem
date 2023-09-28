62745c2b9b111d814f344c2efb3c5dd6dcd37d36
Simple Codes <simplecodes2580@gmail.com>
2023-09-28 12:58:04 -0700

    add routes and create pages

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Box } from '@mui/system'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Profile from './pages/Profile'
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/sign-up' element = {<Signup/>} />
+        <Route path='/sign-in' element = {<Signin/>} />
+        <Route path='/profile' element = {<Profile/>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
