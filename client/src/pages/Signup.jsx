3e79587dec5a843348b9d9c574093ab7f9c1736e
Simple Codes <simplecodes2580@gmail.com>
2023-09-28 16:20:15 -0700

    sign-up page ui developed

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,36 @@
+import { Box, Button, FormControl, InputBase, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'32px'}>
+        <Typography pb={'8px'} variant='h4'>Signup</Typography>
+        <form style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "300px",
+            gap: "8px"
+        }}>
+            <InputBase sx={{bgcolor: 'lightgrey', borderRadius: "5px", p: "8px"}}placeholder='Username' id='username'/>
+
+            <InputBase sx={{bgcolor: 'lightgrey', borderRadius: "5px", p: "8px"}}placeholder='Email' id='email'/>
+
+            <InputBase sx={{bgcolor: 'lightgrey', borderRadius: "5px", p: "8px"}}placeholder='Password' id='password'/>   
+
+            <Button sx={{
+                bgcolor: 'darkslateblue',
+                color: "white",
+                "&:hover" : {
+                    color: "darkslateblue"
+                }
+            }}>Sign up</Button>
+        </form>
+        <Typography mt={'32px'}>Have an account</Typography>
+        <Link to={'/sign-in'} style={{textDecoration: "none"}}>
+            <span>Sign in</span>
+        </Link>
+    </Box>
   )
 }
 
