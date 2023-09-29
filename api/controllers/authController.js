8c18d34de0cf05af2441fe39031be840130b01a3
Simple Codes <simplecodes2580@gmail.com>
2023-09-29 10:07:45 -0700

    create signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,10 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import { errorHandler } from "../utils/error.js"
+
+dotenv.config()
 
 export const signUp = async (req,res, next) => {
     const {username, email, password} = req.body
@@ -11,6 +16,24 @@ export const signUp = async (req,res, next) => {
             message: "user created successfully"
         })
     } catch (error) { 
+        next(error)
+    }
+}
+
+export const signIn = async(req,res,next) => {
+    const {email, password} = req.body
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(404, "User not found"))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return next(errorHandler(401, "Wrong credentials"))
+
+        const token = jwt.sign({_id: validUser._id},process.env.JWT_SECRET)
+        
+        const {password: hashedPassword, ...rest} = validUser._doc
+        const expiryDate = new Date(Date.now() + 3600000)
+        res.cookie('access_token',token, {httpOnly: true, expires: expiryDate}).status(200).json(rest)
+    } catch (error) {
         next(error)
     }
 }
