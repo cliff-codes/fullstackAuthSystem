e0160e1ea4a95746a11bd9c07247990d15a1b157
Simple Codes <simplecodes2580@gmail.com>
2023-10-02 13:37:50 -0700

    add image for profile functionality (bug fixed)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,33 +17,41 @@ const Profile = () => {
   useEffect(() => {
     if(image){
       handleProfilePicUpload(image)
+      console.log(formData)
     }
   },[image])
 
 
   const handleProfilePicUpload = (image) => {
-    console.log(image)
-    const storage = getStorage(app)
-    const fileName = new Date().getTime() + image.name
-    const storageRef = ref(storage, fileName)
-    const uploadTask = uploadBytesResumable(storageRef,image)
-    uploadTask.on('state_changed',
+    console.log(image);
+  
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+  
+    uploadTask.on(
+      'state_changed',
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        setUploadPercentage(Math.round(progress)) 
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPercentage(Math.round(progress));
+      },
+      (error) => {
+        setUploadError(true);
+        console.error('Error uploading file:', error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setFormData({ ...formData, profilePicture: downloadUrl });
+          console.log(formData);
+        });
       }
-    )
-    error => {
-      setUploadError(true)
-    }
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-        setFormData({...formData, profilePicture: downloadUrl})
-      })
-    }
-  }
+    );
+  };
+  
 
-
+  console.log(formData)
+  
   return (
     <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'32px'}>
       <Typography variant='h5'>Profile</Typography>
@@ -51,13 +59,13 @@ const Profile = () => {
         <input type='file' ref={fileRef} hidden accept='image/*'
           onChange={(e) => setImage(e.target.files[0])}
         />
-        <Box>
-          <img onClick={() => fileRef.current.click()} width={'80px'} height={'80px'} style={{borderRadius: "50%"}} src={`${currentUser.profilePicture}`} />
+        <Box display={'flex'} justifyContent={'center'} flexDirection={'column'} alignItems={'center'}>
+          <img onClick={() => fileRef.current.click()} width={'80px'} height={'80px'} style={{borderRadius: "50%", backgroundPosition: "center", backgroundSize: "cover"}} src={`${formData.profilePicture ||currentUser.profilePicture}`} />
           <div>
             {uploadError ? <span>
               <Typography color={'lightcoral'}>Error uploading image</Typography>
-            </span>: uploadPercentage > 0 && uploadPercentage < 100 ? <Typography textAlign={'center'}> {`image uploading ${uploadPercentage}%`}</Typography>:
-            <Typography color={'lightgreen'}>image uploaded successfully</Typography>
+            </span>: uploadPercentage > 0 && uploadPercentage < 100 ? <Typography textAlign={'center'}> {`image uploading ${uploadPercentage}%`}</Typography>: uploadPercentage == 100 ?
+            <Typography color={'lightgreen'}>image uploaded successfully</Typography> : null
           }
           </div>
         </Box>
