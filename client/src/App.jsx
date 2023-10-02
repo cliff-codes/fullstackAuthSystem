defeb33b1a7231e5b4663a1e3a97b914d0134d1f
Simple Codes <simplecodes2580@gmail.com>
2023-10-02 09:00:33 -0700

    complete profile page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 
-import { Box } from '@mui/system'
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,31 @@
+import { Box, Button, ButtonBase, InputBase, Typography } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector(state => state.user)
   return (
-    <div>Profile</div>
+    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'32px'}>
+      <Typography variant='h5'>Profile</Typography>
+      <form style={{display: "flex", flexDirection: 'column', alignItems: "center", gap: "16px", marginTop: "32px"}}>
+        <Box>
+          <img width={'80px'} height={'80px'} style={{borderRadius: "50%"}} src={`${currentUser.profilePicture}`} />
+        </Box>
+        <Box display={'flex'} flexDirection={'column'}>
+          <InputBase defaultValue={currentUser.username} id='username'  sx={{borderRadius: "4px",minWidth: "300px",bgcolor: "lightgrey", mb: "8px", p:"8px"}} placeholder='username'/>
+          <InputBase defaultValue={currentUser.email} id='email'  sx={{borderRadius: "4px",minWidth: "300px",bgcolor: "lightgrey", mb: "8px", p:"8px"}} placeholder='email'/>
+          <InputBase id='password'  sx={{borderRadius: "4px",minWidth: "300px",bgcolor: "lightgrey", mb: "8px", p:"8px"}} placeholder='password'/>
+        </Box>
+        <Button sx={{bgcolor:'#040720',color: 'white', width: "100%", ":hover": {
+          color: '#040720',
+          // border: "1px solid #040720"
+        }}}>update</Button>
+        <Box width={'100%'} display={'flex'} justifyContent={'space-between'}>
+          <ButtonBase sx={{color:'lightcoral', fontWeight: '600'}}>Delete Account</ButtonBase>
+          <ButtonBase sx={{color:'lightcoral', fontWeight: '600'}}>Sign out</ButtonBase>
+        </Box>
+      </form>
+    </Box>
   )
 }
 
