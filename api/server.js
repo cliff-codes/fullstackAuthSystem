bce3df228d4a25d1720594b13621dc2d1cb87f51
Simple Codes <simplecodes2580@gmail.com>
2023-10-03 16:28:47 -0700

    add pre production changes

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/userRoute.js'
 import authRouter from './routes/auth.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -11,12 +13,18 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch(err => {
     console.log(err)  
 })
+const __dirname = path.resolve()
 
 const app = express()
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/user", userRoutes)
 app.use('/api/auth', authRouter)
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
