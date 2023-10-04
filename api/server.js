b8d55f9b351f4ad270f8cf18419f7d82fd267b35
Simple Codes <simplecodes2580@gmail.com>
2023-10-04 12:26:59 -0700

    config update

--- api/server.js
+++ api/server.js
@@ -13,13 +13,9 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch(err => {
     console.log(err)  
 })
-const __dirname = path.resolve()
 
 const app = express()
-// app.use(express.static(path.join(__dirname, '/client/dist')))
-// app.use('*', (req,res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-// })
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/user", userRoutes)
